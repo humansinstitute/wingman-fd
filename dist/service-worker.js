e01a3cbb3f92b61b07e37a34c9cc8e40a71e3828
honest-ivory-thicket <honest-ivory-thicket@wingman-os.ai>
2026-04-28 11:44:28 +0800

    Constrain comments drawer below app header

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260428-0339-6-741";
+const BUILD_ID = "20260428-0344-7-742";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
