02d178ab75bb435552a570a353dd72f0ac779e01
honest-ivory-thicket <honest-ivory-thicket@wingman-os.ai>
2026-05-05 17:45:08 +0800

    Make doc comments drawer reflow layout

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,5 +1,5 @@
 
-const BUILD_ID = "20260505-0931-14-810";
+const BUILD_ID = "20260505-0944-15-811";
 const CACHE_PREFIX = 'wingman-fd';
 const CACHE_NAME = `${CACHE_PREFIX}-${BUILD_ID}`;
 const PRECACHE_URLS = [
